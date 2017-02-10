a5b16068cb82791280db2565e68c59a984243ffa
George Babayan <ggbabayan@gmail.com>
2017-02-10 11:11:28 -0800

    changed req file'

--- print_bot_id.py
+++ print_bot_id.py
@@ -12,10 +12,8 @@ if __name__ == "__main__":
     if api_call.get('ok'):
         # retrieve all users so we can find our bot
         users = api_call.get('members')
-        print(users)
         for user in users:
             if 'name' in user and user.get('name') == BOT_NAME:
                 print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
     else:
-        print("could not find bot user with the name " + BOT_NAME)
         print("could not find bot user with the name " + BOT_NAME)
